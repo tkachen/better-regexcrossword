15a5039158f61a3533d40c4d7b30dd0b28315395
Sergei Tkachenko <2955349+tkachen@users.noreply.github.com>
2024-02-17 05:05:32 +0400

    Add links to regex101 and processing of regex chars

--- better-regexcrossword.user.js
+++ better-regexcrossword.user.js
@@ -2,7 +2,7 @@
 // @name            BetterRegexcrossword
 // @name:ru         BetterRegexcrossword
 // @namespace       https://github.com/tkachen/better-regexcrossword
-// @version         0.2.3
+// @version         0.2.4
 // @description     Adds filters and sort options for player puzzles on regexcrossword.com
 // @description:ru  Добавляет фильтры и сортировки списка головоломок на regexcrossword.com
 // @author          tkachen
@@ -69,6 +69,13 @@
         display: none;
       }
 
+      .regex,
+      .regex * {
+        -webkit-user-select: text;
+        -moz-user-select: text;
+        user-select: text;
+      }
+
       .customClue {
         white-space: nowrap;
 
@@ -77,6 +84,26 @@
         }
       }
 
+      .customPanelClue {
+        display: flex;
+        align-items: center;
+
+        &, & * {
+          font-weight: normal; font-style: normal; text-decoration: none;
+        }
+      }
+
+      .infoLink {
+        margin-right: 10px;
+        color: black;
+
+        & svg {
+          height: 24px;
+          width: 24px;
+        }
+      }
+
+      [data-testid="clues"],
       [data-testid="puzzle"] {
         & tbody th:last-child {
           text-align: left;
@@ -89,7 +116,11 @@
         & .${clueWrapperActiveClass}.${clueWrapperClass},
         & .${clueWrapperActiveClass} .${clueWrapperClass} {
           background-color: hsla(var(--white),90%);
+        }
 
+        & .customPanelClue,
+        & .${clueWrapperActiveClass}.${clueWrapperClass},
+        & .${clueWrapperActiveClass} .${clueWrapperClass} {
           & .regex       {1font-family: Monospace;}
           & .regex b     {background: #99beff99; color: #000000;} /* metasequence */
           & .regex i     {background: #ffc08099; color: #000000;} /* char class */
@@ -101,6 +132,14 @@
           & .regex b.g4  {background: #ceae6399; color: #000000;} /* group: depth 4 */
           & .regex b.g5  {background: #CE896399; color: #000000;} /* group: depth 5 */
           & .regex b.err {background: #ed5c6599; color: #ffffff;} /* error */
+          & .regex .codedChar { background: #ff8def99; }
+          & .regex .nonPrintable {
+            display: inline-block;
+            vertical-align: middle;
+            height: 20px;
+            min-width: 10px;
+            border: 1px solid black;
+          }
         }
 
         ${isHexagonal
@@ -127,7 +166,8 @@
     unsolved: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>\n',
     arrow: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>\n',
     hexagon: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="m20 16c-.001.121-.025.246-.07.362-.08.206-.225.384-.426.5l-6.999 3.999c-.112.064-.232.105-.355.124-.218.034-.445-.003-.654-.124l-6.991-3.995c-.111-.065-.207-.148-.285-.245-.139-.171-.22-.385-.22-.621v-7.993c.001-.128.025-.253.07-.369.08-.206.225-.384.426-.5l6.999-3.999c.112-.064.232-.105.355-.124.218-.034.445.003.654.124l6.991 3.995c.111.065.207.148.285.245.139.171.22.385.22.621zm2 0v-8c-.001-.71-.248-1.363-.664-1.878-.23-.286-.512-.528-.831-.715l-7.009-4.005c-.61-.352-1.3-.465-1.954-.364-.363.057-.715.179-1.037.363l-7.001 4.001c-.618.357-1.06.897-1.299 1.514-.133.342-.202.707-.205 1.077v8.007c.001.71.248 1.363.664 1.878.23.286.512.528.831.715l7.009 4.005c.61.352 1.3.465 1.954.364.363-.057.715-.179 1.037-.363l7.001-4.001c.618-.357 1.06-.897 1.299-1.514.133-.342.202-.707.205-1.084z"></path></svg>\n',
-    keyboard: '<svg height="24" viewBox="0 0 27 24" width="27" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="m25.5 3h-24c-.825 0-1.5.675-1.5 1.5v15c0 .825.675 1.5 1.5 1.5h24c.825 0 1.5-.675 1.5-1.5v-15c0-.825-.675-1.5-1.5-1.5zm-10.5 3h3v3h-3zm4.5 4.5v3h-3v-3zm-9-4.5h3v3h-3zm4.5 4.5v3h-3v-3zm-9-4.5h3v3h-3zm4.5 4.5v3h-3v-3zm-7.5-4.5h1.5v3h-1.5zm0 4.5h3v3h-3zm1.5 7.5h-1.5v-3h1.5zm13.5 0h-12v-3h12zm6 0h-4.5v-3h4.5zm0-4.5h-3v-3h3zm0-4.5h-4.5v-3h4.5z"></path></svg>\n'
+    keyboard: '<svg height="24" viewBox="0 0 27 24" width="27" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="m25.5 3h-24c-.825 0-1.5.675-1.5 1.5v15c0 .825.675 1.5 1.5 1.5h24c.825 0 1.5-.675 1.5-1.5v-15c0-.825-.675-1.5-1.5-1.5zm-10.5 3h3v3h-3zm4.5 4.5v3h-3v-3zm-9-4.5h3v3h-3zm4.5 4.5v3h-3v-3zm-9-4.5h3v3h-3zm4.5 4.5v3h-3v-3zm-7.5-4.5h1.5v3h-1.5zm0 4.5h3v3h-3zm1.5 7.5h-1.5v-3h1.5zm13.5 0h-12v-3h12zm6 0h-4.5v-3h4.5zm0-4.5h-3v-3h3zm0-4.5h-4.5v-3h4.5z"></path></svg>\n',
+    info: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="m12 6q1.641 0 2.813 1.172t1.172 2.813q0 1.266-1.5 2.602t-1.5 2.414h-1.969q0-1.078.469-1.852t1.031-1.125 1.031-.867.469-1.172q0-.797-.609-1.383t-1.406-.586-1.406.586-.609 1.383h-1.969q0-1.641 1.172-2.813t2.813-1.172zm0 14.016q3.281 0 5.648-2.367t2.367-5.648-2.367-5.648-5.648-2.367-5.648 2.367-2.367 5.648 2.367 5.648 5.648 2.367zm0-18q4.125 0 7.055 2.93t2.93 7.055-2.93 7.055-7.055 2.93-7.055-2.93-2.93-7.055 2.93-7.055 7.055-2.93zm-.984 15.984v-2.016h1.969v2.016z"></path></svg>\n',
   }
 
   const sortOptions = {
@@ -379,11 +419,69 @@
 
     $('input[readonly]', puzzle).each(function(){
       $(this.parentElement).append(`
-        <div class="${this.className} customClue regex">${colorizer.colorizeText(this.getAttribute('value'))}</div>
+        <div class="${this.className} customClue regex">${processRegexChars(colorizer.colorizeText(this.getAttribute('value')))}</div>
       `)
       $(this).addClass('originalClue')
     })
+
+    $('.nonPrintable', puzzle).on('click', function() {
+      copyToClipboard(this.textContent)
+    })
+
+    function replaceClueWithCustom(container) {
+      const regexContent = container.textContent.trim();
+      if (!regexContent.length) return
+      container.textContent = ''
+      $(container).addClass('customPanelClue').append(`
+        <a href="https://regex101.com/?regex=${encodeURIComponent(regexContent)}&flavor=javascript&flags=gmi"
+           class="infoLink"
+           target="_blank"
+           rel="noreferrer"
+           title="Go to regex101.com"
+        >${svgIcons.info}</a>
+        <div class="regex">${processRegexChars(colorizer.colorizeText(regexContent))}</div>
+      `)
+
+      $('.nonPrintable', container).on('click', function() {
+        copyToClipboard(this.textContent)
+      })
+    }
+
+    function callback(mutationList) {
+      mutationList.forEach((mutation) => {
+        // on user select new cell game replaces custom clue with new regex clue text
+        if (mutation.addedNodes.length === 1) {
+          replaceClueWithCustom(mutation.target)
+        }
+      });
+    }
+
+    var clues = document.querySelector('[data-testid="clues"]')
+
+    $('[data-testid^="clue-"] > div:not(:first-child)', clues).each(function() {replaceClueWithCustom(this)})
+
+    const observer = new MutationObserver(callback);
+    observer.observe(clues, {subtree: true, childList: true});
   }
+
+  function processRegexChars(str) {
+    // this is rough version to replace octal codes. it begins from 2 digits to avoid replacement of subpattern matches like \1 \2
+    str = str.replace(/\\([0-3]?[0-7]{2})/g,(_, g1) => `<span class="codedChar" title="\\${g1}">${String.fromCharCode(parseInt(g1, 8))}</span>`)
+    str = str.replace(/\\u([0-9a-fA-F]{4})/g, (_, g1) => `<span class="codedChar" title="\\u${g1}">${String.fromCharCode(parseInt(g1, 16))}</span>`)
+    str = str.replace(/\\x([0-9a-fA-F]{2})/g, (_, g1) => `<span class="codedChar" title="\\x${g1}">${String.fromCharCode(parseInt(g1, 16))}</span>`)
+    str = str.replace(/([\u0000-\u001F\u00AD])/g, '<span class="nonPrintable" title="$1">$1</span>')
+    // some zero-width chars
+    str = str.replace(/([\u200b\u200c\u200d\ufeff\u2028\u2029])/g, (_, g1) => `<span class="nonPrintable" title="\\u${g1.charCodeAt(0).toString(16)}">${g1}</span>`)
+    return str
+  }
+
+  async function copyToClipboard(str) {
+    try {
+      await navigator.clipboard.writeText(str);
+    } catch (err) {
+      console.error('Failed to copy!', err);
+    }
+  };
 
   let colorizer = new window.RegexColorize.default()
   let isHexagonal = null
